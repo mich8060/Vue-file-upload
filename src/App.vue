4ffb6a55829a1a8c0f75f21e1845ada44f3dca49
Michael Stevens <mich8060@hotmail.com>
2018-07-05 15:23:06 -0600

    Adding initial setup.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <UploadFile />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import UploadFile from './components/UploadFile.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    UploadFile,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+body {
+    font-family: 'Open-sans',Arial,Sans-serif;
+    font-size:18px;
 }
+
 </style>
---
+++ src/components/UploadFile.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="upload">
+        <h1>Hello upload world!</h1>
+        <form action="">
+            <input type="upload" />
+            <input type="submit" value="Upload File" />
+        </form>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'UploadFile'
+}
+
+</script>
+
+<style lang="scss">
+
+
+
+</style>
